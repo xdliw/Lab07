b390b990e011518540cdcdb785619c2eebe9aeba
xdliw <90445100+xdliw@users.noreply.github.com>
2023-12-25 01:54:14 +0300

    Initial commit

---
+++ Calculations/IntegralCalculation.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace Calculations
+{
+    public class IntegralCalculation
+    {
+        public static Semaphore semaphore = new Semaphore(2, 2);
+        public delegate double Function(double x);
+        private Function function;
+
+        public delegate void NotifyDelegate(double x);
+        public event NotifyDelegate? CompletionPercentEvent;
+        public event NotifyDelegate? EndEvent;
+        public event NotifyDelegate? ElapsedTimeEvent;
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public double Step { get; set; }
+
+        public void Integrate()
+        {
+            semaphore.WaitOne();
+            double result = 0;
+            var cw = Stopwatch.StartNew();
+            double completionPercent = 0;
+            for (double i = A; i <= B; i += Step)
+            {
+                result += function(i) * Step;
+                if (completionPercent + 0.1 < (i - A) / (B - A) * 100)
+                {
+                    completionPercent = (i - A) / (B - A) * 100;
+                    CompletionPercentEvent?.Invoke(completionPercent);
+                }
+                
+            }
+            cw.Stop();
+            ElapsedTimeEvent?.Invoke(cw.ElapsedMilliseconds);
+            EndEvent?.Invoke(result);
+            semaphore.Release();
+        }
+
+        public IntegralCalculation()
+        {
+            this.function = Math.Sin;
+            A = 0;
+            B = 1;
+            Step = 0.00000001;
+        }
+
+        public void SetFunction(Function function)
+        {
+            this.function = function;
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,71 @@
+﻿using Calculations;
+
+
+IntegralCalculation calc = new IntegralCalculation();
+calc.Step = 0.00000001;
+calc.CompletionPercentEvent += (d) =>
+{
+    int id = Thread.CurrentThread.ManagedThreadId;
+    int progBarCells = (int)(d + 0.01) / 5;
+    lock (Console.Out)
+    {
+        Console.SetCursorPosition(0, id);
+        Console.Write($"Stream {id}: [{"=".Repeat(progBarCells)}{">".Repeat((progBarCells < 20) ? 1 : 0)}{" ".Repeat(19 - progBarCells)}]{d:F1}%");
+    }
+};
+
+calc.EndEvent += (result) =>
+{
+    int id = Thread.CurrentThread.ManagedThreadId;
+    lock (Console.Out)
+    {
+        Console.SetCursorPosition(0, id);
+        Console.Write($"Stream {id}: Ended with result: {result}");
+    }
+};
+
+calc.ElapsedTimeEvent += (time) =>
+{
+    int id = Thread.CurrentThread.ManagedThreadId;
+    lock (Console.Out)
+    {
+        Console.SetCursorPosition(60, id);
+        Console.Write($"Consumed time: {time}ms");
+        if (Thread.CurrentThread.Priority == ThreadPriority.Lowest)
+        {
+            Console.Write(" Low priority");
+        }
+        if (Thread.CurrentThread.Priority == ThreadPriority.Highest)
+        {
+            Console.Write(" High priority");
+        }
+    }
+};
+
+Thread thread1 = new Thread(calc.Integrate);
+thread1.Priority = ThreadPriority.Highest;
+thread1.Start();
+
+Thread thread2 = new Thread(calc.Integrate);
+thread2.Priority = ThreadPriority.Lowest;
+thread2.Start();
+
+Thread thread3 = new Thread(calc.Integrate);
+thread3.Start();
+
+Thread thread4 = new Thread(calc.Integrate);
+thread4.Start();
+
+Thread thread5 = new Thread(calc.Integrate);
+thread5.Start();
+
+
+
+public static class StringExtensions
+{
+    public static string Repeat(this string value, int count)
+    {
+        count = Math.Max(0, count);
+        return string.Concat(Enumerable.Repeat(value, count));
+    }
+}
